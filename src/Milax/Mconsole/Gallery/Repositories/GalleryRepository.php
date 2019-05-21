1530f75e960b92966c8a48ff116f283c5aa0b50b
rudenko-v <rudenko@arkout.ru>
2019-05-21 12:01:00 +1100

    Update repository: enable get by tag name

--- src/Milax/Mconsole/Gallery/Repositories/GalleryRepository.php
+++ src/Milax/Mconsole/Gallery/Repositories/GalleryRepository.php
@@ -4,8 +4,11 @@ namespace Milax\Mconsole\Gallery\Repositories;
 
 use Milax\Mconsole\Repositories\EloquentRepository;
 use Milax\Mconsole\Gallery\Contracts\Repositories\GalleryRepository as Repository;
+use Milax\Mconsole\Traits\Repositories\TaggableRepository;
 
 class GalleryRepository extends EloquentRepository implements Repository
 {
+    use TaggableRepository;
+    
     public $model = \Milax\Mconsole\Gallery\Models\Gallery::class;
 }
